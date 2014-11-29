e9239604b3ad81bec0c2f3b6c5a53c8e7ff3cc8f
burakicel <burakicel96@gmail.com>
2014-11-28 21:52:00 -0500

    Distance Function Added

--- epsilondelta.rb
+++ epsilondelta.rb
@@ -11,11 +11,33 @@ def function_eval(f,i)
   eval(f.gsub("x",i.to_s))
 end
 
+#
+# (distance) : Distance Calculator
+# Calculates the distances of two numbers, a and b
+# distance_x Num Num -> Num
+# Example : (distance -2 3) -> 5
+
+def distance(a,b)
+	(a-b).abs
+end
+
+
+
+#Tests
 class TestMeme < Minitest::Test
+
 	def test_function_eval
   	assert_equal -1, function_eval("(Math.cos x)",Math::PI)
   	assert_equal -1, function_eval("(Math.cos x)",3*Math::PI)
   	assert_equal 2, function_eval("1+1",302)
   	assert_equal 1, function_eval("(x+4).abs",-5)
 	end
+
+	def test_distance
+  	assert_equal 1, distance(2,3)
+  	assert_equal 5, distance(-2,3)
+  	assert_in_delta 0.1, distance(2.9,3), 0.000000001
+  	assert_equal 1, distance(3,2)
+	end
+
 end
